07bd3d84c81eb82567bb60d188fafc9b9e5c8357
Liam Bowers <liamondrop@gmail.com>
2016-01-05 16:46:01 -0500

    rename rootReducers -> reducers

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
-import rootReducer from './reducers';
+import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(
   thunkMiddleware, createLogger())(createStore);
 
 export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(rootReducer, initialState);
+  const store = createStoreWithMiddleware(reducers, initialState);
   return store;
 }
