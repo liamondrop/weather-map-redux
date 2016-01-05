24836895377bb928ba95c0c8f2458c1d251329f6
Liam Bowers <liamondrop@gmail.com>
2016-01-05 16:41:25 -0500

    delete unused crap

--- src/index.js
+++ src/index.js
@@ -6,36 +6,9 @@ import {Provider} from 'react-redux';
 import Main from './pages/Main';
 import configureStore from './configureStore';
 
-
 ReactDOM.render(
   <Provider store={configureStore()}>
     <Main/>
   </Provider>,
   document.getElementById('root')
 );
-
-
-// import {extend} from 'underscore';
-// import app from 'ampersand-app';
-// import Router from './router';
-// import GoogleMapsLoader from 'google-maps';
-
-// app.extend({
-//   init() {
-//     this.state = {};
-//     this.router = new Router();
-//     this.router.history.start({
-//       hashChange: false
-//     });
-//     return this.state;
-//   }
-// });
-
-// // load Google maps api. Triggers callback when api is loaded
-// GoogleMapsLoader.LIBRARIES = ['places', 'geometry'];
-// GoogleMapsLoader.load(function (google) {
-//   app.state.google = google;
-//   app.router.render(app.state);
-// });
-
-// window.app = app.init();
