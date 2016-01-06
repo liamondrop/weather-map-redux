8f9b571af1e5205610b0513485953abf738c4427
Liam Bowers <liamondrop@gmail.com>
2016-01-05 20:19:41 -0500

    request weather data action

--- src/actions/request-weather-data.js
+++ src/actions/request-weather-data.js
@@ -1,8 +1,71 @@
+import {map} from 'underscore';
+import * as actions from '../constants/action-types';
+import * as mapOpts from '../constants/map-options';
 
-import {actions} from '../constants';
+const xhr = new XMLHttpRequest();
 
-var xhr = new XMLHttpRequest();
-// xhr.onload = proccessResults;
+function handleResponse(dispatch, getState) {
+  return function onXHRLoad() {
+    try {
+      const data = JSON.parse(xhr.responseText || '{}');
+      const features = map(data.list, (item) => ({
+        type: 'Feature',
+        properties: {
+          city: item.name,
+          weather: item.weather[0].main,
+          temperature: item.main.temp,
+          min: item.main.temp_min,
+          max: item.main.temp_max,
+          humidity: item.main.humidity,
+          pressure: item.main.pressure,
+          windSpeed: item.wind.speed,
+          windDegrees: item.wind.deg,
+          windGust: item.wind.gust,
+          icon: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`,
+          coordinates: [item.coord.lon, item.coord.lat]
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [item.coord.lon, item.coord.lat]
+        }
+      }));
+      dispatch({
+        type: actions.WEATHER_DATA_RECEIVED,
+        geoJSON: {
+          type: 'FeatureCollection',
+          features
+        }
+      });
+    } catch (error) {
+      console.error(error.stack);
+    }
+  }
+  // if (results.list.length > 0) {
+  //   resetData();
+  //   for (var i = 0; i < results.list.length; i++) {
+  //     geoJSON.features.push(jsonToGeoJson(results.list[i]));
+  //   }
+  //   drawIcons(geoJSON);
+  // }
+}
+
+function drawIcons(weather) {
+  map.data.addGeoJson(geoJSON);
+  gettingData = false;
+}
+
+// Clear data layer and geoJSON
+function resetData() {
+  geoJSON = {
+    type: 'FeatureCollection',
+    features: []
+  };
+  map.data.forEach(function (feature) {
+    map.data.remove(feature);
+  });
+}
+
+
 
 // function requestWeatherData() {
 //   return {
@@ -16,29 +79,39 @@ var xhr = new XMLHttpRequest();
 //   };
 // }
 
-// function fetchPosts(reddit) {
-//   return dispatch => {
-//     dispatch(requestPosts(reddit))
-//     return fetch(`http://www.reddit.com/r/${reddit}.json`)
-//       .then(response => response.json())
-//       .then(json => dispatch(receivePosts(reddit, json)))
-//   }
-// }
+// For each result that comes back, convert the data to geoJSON
+function toGeoJson(weatherItem) {
+  return ;
+
+  // Set the custom marker icon
+  // map.data.setStyle(function (feature) {
+  //   return {
+  //     icon: {
+  //       url: feature.getProperty('icon'),
+  //       anchor: new google.maps.Point(25, 25)
+  //     }
+  //   };
+  // });
+
+  // returns object
+  return feature;
+};
 
 // Make the weather request
 function getWeather() {
   return (dispatch, getState) => {
-    const {mapBounds} = getState();
+    const {map, mapBounds} = getState();
+    const {westLng, northLat, eastLng, southLat} = mapBounds;
+    const requestUrl = `${mapOpts.OWM_API_BASE}/box/city?&cluster=yes&format=json`
+                      + `&APPID=${window.OWM_APP_ID}&bbox=`
+                      + `${westLng},${northLat},` // left top
+                      + `${eastLng},${southLat},` // right bottom
+                      + map.getZoom();
+    xhr.onload = handleResponse(dispatch, getState);
+    xhr.open('get', requestUrl, true);
+    xhr.send();
+    dispatch({type: actions.REQUEST_WEATHER_DATA});
   };
-  // this.setState({gettingData: true})
-  // var requestString = 'http://api.openweathermap.org/data/2.5/box/city?bbox='
-  //                     + westLng + ',' + northLat + ',' //left top
-  //                     + eastLng + ',' + southLat + ',' //right bottom
-  //                     + map.getZoom()
-  //                     + '&cluster=yes&format=json'
-  //                     + '&APPID=' + '551b97ca557560dfc7d8c49a81b37d89';
-  // xhr.open('get', requestString, true);
-  // xhr.send();
 }
 
 export default function requestWeatherData() {
