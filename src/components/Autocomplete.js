0b665c5adcfac88e0afa1e9f6fdcd379acf02bb6
Liam Bowers <liamondrop@gmail.com>
2016-01-20 13:22:53 -0500

    autocomplete component

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,12 +1,28 @@
+import {map} from 'underscore';
 import React from 'react';
 
-export default function Autocomplete(props) {
-  return (
-    <div className="form-group autocomplete">
-      <input id="autocomplete"
-        type="text"
-        className="form-control"
-        onKeyUp={props.onSearch}/>
-    </div>
-  );
+export default class Autocomplete extends React.Component {
+  render() {
+    const {predictions} = this.props;
+    return (
+      <div className={`dropdown autocomplete${predictions.length ? ' open' : ''}`}>
+        <div className="input-group input-group-lg">
+          <input id="autocomplete"
+            type="text"
+            className="form-control"
+            onKeyUp={this.props.onSearch}/>
+          <span className="input-group-addon">@</span>
+        </div>
+        <ul onClick={this.props.onPredictionSelect} className="dropdown-menu">
+          {predictions.map((p, index) => {
+            return (
+              <li key={index}>
+                <a href={`/?place=${p.place_id}`}>{p.description}</a>
+              </li>
+              );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
