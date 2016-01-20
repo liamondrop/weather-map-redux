af368b1e1889b139b819506da60884144b2e496e
Liam Bowers <liamondrop@gmail.com>
2016-01-20 13:23:07 -0500

    updates to Main class

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,46 +1,87 @@
 import React from 'react';
-import local from 'local-links';
-import {bindAll, debounce} from 'underscore';
+import {bindAll, debounce, isEmpty} from 'underscore';
 import {connect} from 'react-redux';
-import GoogleMap from '../components/Map';
+import {createHistory, useQueries} from 'history';
 import Autocomplete from '../components/Autocomplete';
 import initGoogleMaps from '../actions/init-google-maps';
-import updateAutocompletePredictions from '../actions/update-autocomplete-predictions';
+import * as autocompletePredictions from '../actions/update-autocomplete-predictions';
+
+const history = useQueries(createHistory)();
 
 class Main extends React.Component {
   constructor() {
     super();
-    bindAll(this, '_onClick', '_onSearch');
+    bindAll(this, '_onHistoryChange', '_onSearch',
+      '_onPredictionsReturn', '_onPredictionSelect');
+    this.state = {unlisten: null};
+  }
+
+  componentWillReceiveProps() {
+    // defer listening to browser history until geocoder
+    // has initiated in order to set the map location if
+    // `location.query.place` is set
+    if (this.state.unlisten === null &&
+      typeof this.props.geocoder.geocode === 'function') {
+      this.setState({
+        unlisten: history.listen(this._onHistoryChange)
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    this.state.unlisten();
   }
 
   componentDidMount() {
-    const {dispatch} = this.props;
-    dispatch(initGoogleMaps());
+    this.props.dispatch(initGoogleMaps());
   }
 
-  // delegate click handling of local
-  // links to the client side router
-  _onClick(e) {
-    const pathName = local.pathname(e);
-    if (pathName) {
-      e.preventDefault();
-      console.log(pathName);
-      // app.router.navigate(pathName);
+  _onHistoryChange(location) {
+    const {dispatch, geocoder, map, mapsApi} = this.props;
+    const placeId = location.query.place;
+    if (placeId) {
+      geocoder.geocode({placeId}, function(results, status) {
+        if (status === mapsApi.GeocoderStatus.OK) {
+          if (results[0]) {
+            map.setZoom(10);
+            map.setCenter(results[0].geometry.location);
+          }
+        }
+      });
     }
   }
 
   _onSearch(e) {
     const {autocomplete, dispatch, mapsApi} = this.props;
-    autocomplete.getQueryPredictions({input: e.target.value},
-      updateAutocompletePredictions(mapsApi, dispatch));
+    const {value} = e.target;
+    if (value) {
+      autocomplete.getQueryPredictions({input: value},
+        this._onPredictionsReturn);
+    } else {
+      dispatch(autocompletePredictions.clear());
+    }
+  }
+
+  _onPredictionsReturn(predictions, status) {
+    const {dispatch} = this.props;
+    dispatch(autocompletePredictions.update(predictions, status));
+  }
+
+  _onPredictionSelect(event) {
+    event.preventDefault();
+    const {dispatch} = this.props;
+    const matches = event.target.href.match(/.*(\/\?.*)/); // only get the pathname
+    history.push(matches[1]);
+    dispatch(autocompletePredictions.clear()); // clear predictions on select
   }
 
   render() {
     return (
       <div className="base" onClick={this._onClick}>
-        <GoogleMap {...this.props}/>
+        <div ref="map" id="map-canvas" style={{height: '100%'}}/>
         <Autocomplete {...this.props}
-          onSearch={debounce(this._onSearch, 100)}/>
+          onSearch={debounce(this._onSearch, 200)}
+          onPredictionSelect={this._onPredictionSelect}/>
       </div>
     );
   }
