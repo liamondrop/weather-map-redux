adc1c1e893a2749a9eda07080dae6884b50bf550
Liam Bowers <liamondrop@gmail.com>
2016-01-20 13:21:44 -0500

    updates to request weather data action

--- src/actions/request-weather-data.js
+++ src/actions/request-weather-data.js
@@ -1,116 +1,91 @@
-import {map} from 'underscore';
+import _ from 'underscore';
 import * as actions from '../constants/action-types';
 import * as mapOpts from '../constants/map-options';
 
 const xhr = new XMLHttpRequest();
 
-function handleResponse(dispatch, getState) {
-  return function onXHRLoad() {
-    try {
-      const data = JSON.parse(xhr.responseText || '{}');
-      const features = map(data.list, (item) => ({
-        type: 'Feature',
-        properties: {
-          city: item.name,
-          weather: item.weather[0].main,
-          temperature: item.main.temp,
-          min: item.main.temp_min,
-          max: item.main.temp_max,
-          humidity: item.main.humidity,
-          pressure: item.main.pressure,
-          windSpeed: item.wind.speed,
-          windDegrees: item.wind.deg,
-          windGust: item.wind.gust,
-          icon: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`,
-          coordinates: [item.coord.lon, item.coord.lat]
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [item.coord.lon, item.coord.lat]
-        }
-      }));
+function _onXHRLoad(dispatch) {
+  return function handleResponse() {
+    dispatch(clearDataLayer());
+    dispatch(formatResponse(xhr.responseText));
+    dispatch(drawIcons());
+  }
+}
+
+function clearDataLayer() {
+  return (dispatch, getState) => {
+    const {map} = getState();
+    map.data.forEach(function (feature) {
+      map.data.remove(feature);
+    });
+    dispatch({type: actions.CLEAR_DATA_LAYER});
+  };
+}
+
+function formatResponse(responseText='{}') {
+  return (dispatch, getState) => {
+    const data = JSON.parse(responseText);
+    if (_.isArray(data.list)) {
       dispatch({
-        type: actions.WEATHER_DATA_RECEIVED,
+        type: actions.RECEIVE_WEATHER_DATA,
         geoJSON: {
           type: 'FeatureCollection',
-          features
+          features: _.map(data.list, (item) => ({
+            type: 'Feature',
+            properties: {
+              city: item.name,
+              weather: item.weather[0].main,
+              temperature: item.main.temp,
+              min: item.main.temp_min,
+              max: item.main.temp_max,
+              humidity: item.main.humidity,
+              pressure: item.main.pressure,
+              windSpeed: item.wind.speed,
+              windDegrees: item.wind.deg,
+              windGust: item.wind.gust,
+              icon: `${mapOpts.OWM_ICON_BASE}/${item.weather[0].icon}.png`,
+              coordinates: [item.coord.lon, item.coord.lat]
+            },
+            geometry: {
+              type: 'Point',
+              coordinates: [item.coord.lon, item.coord.lat]
+            }
+          }))
         }
       });
-    } catch (error) {
-      console.error(error.stack);
     }
-  }
-  // if (results.list.length > 0) {
-  //   resetData();
-  //   for (var i = 0; i < results.list.length; i++) {
-  //     geoJSON.features.push(jsonToGeoJson(results.list[i]));
-  //   }
-  //   drawIcons(geoJSON);
-  // }
+  };
 }
 
 function drawIcons(weather) {
-  map.data.addGeoJson(geoJSON);
-  gettingData = false;
-}
-
-// Clear data layer and geoJSON
-function resetData() {
-  geoJSON = {
-    type: 'FeatureCollection',
-    features: []
+  return (dispatch, getState) => {
+    const {map, geoJSON} = getState();
+    map.data.addGeoJson(geoJSON);
+    map.data.setStyle((feature) => ({
+      icon: {
+        url: feature.getProperty('icon'),
+        anchor: new google.maps.Point(25, 25)
+      }
+    }));
   };
-  map.data.forEach(function (feature) {
-    map.data.remove(feature);
-  });
 }
-
-
-
-// function requestWeatherData() {
-//   return {
-//     type: actions.REQUEST_WEATHER_DATA
-//   };
-// }
-
-// function weatherDataReceived() {
-//   return {
-//     type: actions.WEATHER_DATA_RECEIVED
-//   };
-// }
-
-// For each result that comes back, convert the data to geoJSON
-function toGeoJson(weatherItem) {
-  return ;
-
-  // Set the custom marker icon
-  // map.data.setStyle(function (feature) {
-  //   return {
-  //     icon: {
-  //       url: feature.getProperty('icon'),
-  //       anchor: new google.maps.Point(25, 25)
-  //     }
-  //   };
-  // });
-
-  // returns object
-  return feature;
-};
 
 // Make the weather request
-function getWeather() {
+function getWeatherData() {
   return (dispatch, getState) => {
     const {map, mapBounds} = getState();
-    const {westLng, northLat, eastLng, southLat} = mapBounds;
-    const requestUrl = `${mapOpts.OWM_API_BASE}/box/city?&cluster=yes&format=json`
-                      + `&APPID=${window.OWM_APP_ID}&bbox=`
-                      + `${westLng},${northLat},` // left top
-                      + `${eastLng},${southLat},` // right bottom
-                      + map.getZoom();
-    xhr.onload = handleResponse(dispatch, getState);
-    xhr.open('get', requestUrl, true);
-    xhr.send();
-    dispatch({type: actions.REQUEST_WEATHER_DATA});
+    if (typeof map.getZoom === 'function') {
+      const {westLng, northLat, eastLng, southLat} = mapBounds;
+      const requestUrl = `${mapOpts.OWM_API_BASE}/box/city?cluster=yes&format=json`
+                        + `&units=imperial&APPID=${window.OWM_APP_ID}&bbox=`
+                        + `${westLng},${northLat},` // left top
+                        + `${eastLng},${southLat},` // right bottom
+                        + map.getZoom();              
+      xhr.onload = _onXHRLoad(dispatch);
+      xhr.open('get', requestUrl, true);
+      xhr.send();
+      dispatch({type: actions.REQUEST_WEATHER_DATA});
+    }
   };
 }
 
@@ -121,6 +96,6 @@ export default function requestWeatherData() {
       xhr.abort();
       dispatch({type: actions.ABORT_WEATHER_DATA_REQUEST});
     }
-    dispatch(getWeather());
+    dispatch(getWeatherData());
   };
 }
