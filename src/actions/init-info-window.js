4a48f305952651cbfa5dc866525f3ad2f3b92b55
Liam Bowers <liamondrop@gmail.com>
2016-01-20 16:19:10 -0500

    info window

--- src/actions/init-info-window.js
+++ src/actions/init-info-window.js
@@ -1,3 +1,47 @@
+import degreesToDirection from '../lib/map-degrees-to-direction';
+
+
+function infoTemplate(feature) {
+  return `
+    <div class="info-window">
+      <h5 class="location">${feature.getProperty('city')}</h5>
+      <div class="row">
+        <div class="col-xs-6">
+          <div class="temp temp-current text-info">
+            ${feature.getProperty('temperature')} &deg;
+            <small class="temp-metric">F</small>
+          </div>
+        </div>
+        <div class="col-xs-6">
+          <div class="current-weather">
+            <img src="${feature.getProperty('icon')}" alt="${feature.getProperty('weather')}"/>
+            <span class="weather-caption">${feature.getProperty('weather')}</span>
+          </div>
+        </div>
+      </div>
+      <div class="hilo-wrapper">
+        <div class="temp temp-hi">
+          High:
+          ${feature.getProperty('high')} &deg;
+          <small class="temp-metric">F</small>
+        </div>
+        <div class="temp temp-lo">
+          Low:
+          ${feature.getProperty('low')} &deg;
+          <small class="temp-metric">F</small>
+        </div>
+      </div>
+      <div class="wind">
+        Wind: ${degreesToDirection(feature.getProperty('windDegrees'))}
+        ${feature.getProperty('windSpeed')} meters/sec
+      </div>
+      <div class="pressure">Pressure: ${feature.getProperty('pressure')} hPa</div>
+      <div class="humidity">Hudity: ${feature.getProperty('humidity')}%</div>
+    </div>
+  `;
+}
+
+
 // Sets up and populates the info window with details
 export default function initInfoWindow() {
   return (dispatch, getState) => {
@@ -7,12 +51,7 @@ export default function initInfoWindow() {
     });
     map.data.addListener('click', (event) => {
       const {feature, latLng} = event;
-      infoWindow.setContent(
-       '<img src=' + feature.getProperty('icon') + '>'
-       + '<br /><strong>' + feature.getProperty('city') + '</strong>'
-       + '<br />' + feature.getProperty('temperature') + '&deg; F'
-       + '<br />' + feature.getProperty('weather')
-       );
+      infoWindow.setContent(infoTemplate(feature));
       infoWindow.setOptions({
         position:{
           lat: latLng.lat(),
