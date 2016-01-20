9e96c1968868df8898956f84fd6e0ef7b2562994
Liam Bowers <liamondrop@gmail.com>
2016-01-20 13:22:02 -0500

    updates to autocomplete predictions action

--- src/actions/update-autocomplete-predictions.js
+++ src/actions/update-autocomplete-predictions.js
@@ -1,14 +1,21 @@
 import * as actions from '../constants/action-types';
 
-export default function updateAutocompletePredictions(mapsApi, dispatch) {
-  const ok = mapsApi.places.PlacesServiceStatus.OK;
-  return (predictions, status) => {
-    if (status != ok) {
-      return;
+export function update(predictions, status) {
+  return (dispatch, getState) => {
+    const {mapsApi} = getState();
+    if (status === mapsApi.places.PlacesServiceStatus.OK) {
+      dispatch({
+        type: actions.UPDATE_AUTOCOMPLETE_PREDICTIONS,
+        predictions
+      });
     }
+  };
+}
+
+export function clear() {
+  return (dispatch) => {
     dispatch({
-      type: actions.UPDATE_AUTOCOMPLETE_PREDICTIONS,
-      predictions
+      type: actions.CLEAR_AUTOCOMPLETE_PREDICTIONS
     });
   };
 }
