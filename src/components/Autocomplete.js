6d2568d50826a632b3b3b85976375e4a05c7a309
Liam Bowers <liamondrop@gmail.com>
2016-01-20 16:20:10 -0500

    updates to autocomplete

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -3,16 +3,18 @@ import React from 'react';
 
 export default class Autocomplete extends React.Component {
   render() {
-    const {predictions} = this.props;
+    const {predictions, placeName} = this.props;
     return (
       <div className={`dropdown autocomplete${predictions.length ? ' open' : ''}`}>
-        <div className="input-group input-group-lg">
+        <div className="input-group">
           <input id="autocomplete"
             type="text"
             className="form-control"
             onKeyUp={this.props.onSearch}
           />
-          <span className="input-group-addon">@</span>
+          <span className="input-group-btn">
+            <a className="btn btn-default">&times;</a>
+          </span>
         </div>
         <ul onClick={this.props.onPredictionSelect} className="dropdown-menu">
           {map(predictions, (p, index) => {
